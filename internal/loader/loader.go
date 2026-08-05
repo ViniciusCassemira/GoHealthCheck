fbeb53c8dae2d0020680c6aab624a454198a4981
vinicius <viniciuscruzc2702@gmail.com>
2026-08-04 22:08:49 -0300

    improve: validate the URL before the search

--- internal/loader/loader.go
+++ internal/loader/loader.go
@@ -1,16 +1,19 @@
 package loader
 
 import (
+	"GoHealthCheck/internal/logger"
+	"GoHealthCheck/internal/output"
+	"encoding/json"
+	"net/url"
 	"os"
 	"strings"
-	"encoding/json"
-	"GoHealthCheck/internal/output"
+	
 )
 
-func LoadUrls(path string) ([]string, error) {
+func ValidateAndLoadUrls(path string) ([]string, error) {
 
 	// Read content file
-	data, err := os.ReadFile(path)
+	data, err := readJFileContent(path)
 	if err != nil {
 		return []string{}, err
 	}
@@ -18,20 +21,25 @@ func LoadUrls(path string) ([]string, error) {
 	// Create Slice with file content
 	urls := strings.Split(string(data), "\n")
 
-	// Remove empty strings
+	// Remove empty strings and invalid urls
 	urls_filtered := []string{}
-	for _, url := range urls {
-		url = strings.TrimSpace(url)
-		if url != "" {
-			urls_filtered = append(urls_filtered, url)
+	for _, current_url := range urls {
+		url_clean := strings.TrimSpace(current_url)
+
+		if url_clean != "" && isValidHttpUrl(url_clean) {
+			urls_filtered = append(urls_filtered, url_clean)
 		}
+	}
+
+	if len(urls_filtered) == 0 {
+		logger.WriteLog("No valid URL found")
 	}
 
 	return urls_filtered, nil
 }
 
 func ConvertJsonConfigFileToStruct(pathFile string) (output.CheckOptions, error) {
-	data, err := LoadJsonConfigFile(pathFile)
+	data, err := readJFileContent(pathFile)
 	if err != nil {
 		return output.CheckOptions{}, err
 	}
@@ -46,11 +54,20 @@ func ConvertJsonConfigFileToStruct(pathFile string) (output.CheckOptions, error)
 	return checkOptions, nil
 }
 
-func LoadJsonConfigFile(path string) ([]byte, error) {
+func readJFileContent(path string) ([]byte, error) {
 	data, err := os.ReadFile(path)
 	if err != nil {
 		return []byte{}, err
 	}
 
 	return data, nil
+}
+
+func isValidHttpUrl(rawUrl string) bool {
+	u, err := url.ParseRequestURI(rawUrl)
+	if err != nil {
+		return false
+	}
+
+	return u.Scheme != "" && u.Host != "" && (u.Scheme == "http" || u.Scheme == "https")
 }
